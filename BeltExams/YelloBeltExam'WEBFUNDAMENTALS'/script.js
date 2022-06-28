5d565646dd5a0a313e69a7d9e2e523f1510750ba
Aziza <alshahraani.a@gmail.com>
2022-06-29 02:29:45 +0300

    update

--- BeltExams/YelloBeltExam'WEBFUNDAMENTALS'/script.js
+++
@@ -1,9 +0,0 @@
-function change(e){
-    e.src="images/assets/succulents-2.jpg"
-}
-function back(e){
-    e.src="images/assets/succulents-1.jpg"
-}
-function hide(e) {
-    e.parentElement.remove();
-}
