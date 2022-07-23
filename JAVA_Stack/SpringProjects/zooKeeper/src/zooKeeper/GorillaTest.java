c155a698810a8735cd8ee71475698d3e435683ec
Aziza Alshahrani <105348844+M-Aziza@users.noreply.github.com>
2022-07-23 23:05:20 +0300

    Update GorillaTest.java

--- JAVA_Stack/SpringProjects/zooKeeper/src/zooKeeper/GorillaTest.java
+++ JAVA_Stack/SpringProjects/zooKeeper/src/zooKeeper/GorillaTest.java
@@ -6,19 +6,19 @@ public class GorillaTest {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Mammal mammal = new Mammal();
+
 		Gorilla gorilla = new Gorilla();
 		
-		mammal.displayEnergy();
+		gorilla.displayEnergy();
 		gorilla.throwSomething();
 		gorilla.throwSomething();
 		gorilla.throwSomething();
-		mammal.displayEnergy();
+		gorilla.displayEnergy();
 		gorilla.eatBananas();
 		gorilla.eatBananas();
-		mammal.displayEnergy();
+		gorilla.displayEnergy();
 		gorilla.climb();
-		mammal.displayEnergy();
+		gorilla.displayEnergy();
 		
 	}
 
