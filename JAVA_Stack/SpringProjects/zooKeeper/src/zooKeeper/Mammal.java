e3aebeafd36e00f90112646ce506bc912aab7fa0
Aziza Alshahrani <105348844+M-Aziza@users.noreply.github.com>
2022-07-23 23:07:45 +0300

    Update Mammal.java

--- JAVA_Stack/SpringProjects/zooKeeper/src/zooKeeper/Mammal.java
+++ JAVA_Stack/SpringProjects/zooKeeper/src/zooKeeper/Mammal.java
@@ -3,14 +3,25 @@ package zooKeeper;
 //Create a Mammal class that has an energyLevel member variable and displayEnergy() method. The displayEnergy() method should show the animal's energy level as well as return it
 
 public class Mammal {
-	protected static int energyLevel = 100;
+	
+	
 
-	public static int getEnergyLevel() {
+	private int energyLevel;
+
+	public Mammal() {
+		this.energyLevel=100;
+	}
+	public  int getEnergyLevel() {
 		return energyLevel;
 	}
+
+	public void setEnergyLevel(int energyLevel) {
+		this.energyLevel = energyLevel;
+	}
+
 	public void displayEnergy() {
 		System.out.println("");
-		System.out.println("The energy level of Gorilla is : "+ energyLevel);
+		System.out.println("The energy level of Gorilla is : "+ getEnergyLevel());
 		System.out.println("");
 	}
 
