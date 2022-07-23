96b931f4042628a12f70d55077801063ab5e2545
Aziza Alshahrani <105348844+M-Aziza@users.noreply.github.com>
2022-07-23 23:07:05 +0300

    Update Gorilla.java

--- JAVA_Stack/SpringProjects/zooKeeper/src/zooKeeper/Gorilla.java
+++ JAVA_Stack/SpringProjects/zooKeeper/src/zooKeeper/Gorilla.java
@@ -2,12 +2,13 @@ package zooKeeper;
 //Create a separate class Gorilla that can throwSomething(), eatBananas(), and climb()
 
 public class Gorilla extends Mammal {
-	
+
 //	 For the throwSomething() method, have it print out a message indicating that the gorilla has thrown something, as well as decrease the energy level by 5
 	
 	public void throwSomething() {
-		energyLevel -=5;
+		int energy = getEnergyLevel() -5;
 		System.out.println("Gorilla thrown somthing ");
+		this.setEnergyLevel(energy);
 		
 		
 	}
@@ -15,15 +16,17 @@ public class Gorilla extends Mammal {
 //	 For the eatBananas() method, have it print out a message indicating the gorilla's satisfaction and increase its energy by 10
 	
 	public void eatBananas() {
-		energyLevel += 10;
+		int energy = getEnergyLevel()+ 10;
 		System.out.println("Gorilla eat Banana  ");
+		this.setEnergyLevel(energy);
 	}
 	
 //	 For the climb() method, have it print out a message indicating the gorilla has climbed a tree and decrease its energy by 10 
 	 
 	public void climb() {
-		energyLevel -= 10;
+		int energy = getEnergyLevel()- 10;
 		System.out.println("Gorilla climbed the tree");
+		this.setEnergyLevel(energy);
 
 		
 	}
